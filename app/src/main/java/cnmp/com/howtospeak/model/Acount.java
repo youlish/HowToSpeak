8875e32363edae2312f68a5c780e323b6638ded4
Tran Duc Dzung <ducdung.96.ydbg@gmail.com>
2017-12-24 15:45:37 +0700

    setup PlayVideoActivity

---
+++ app/src/main/java/cnmp/com/howtospeak/model/Acount.java
@@ -0,0 +1,20 @@
+package cnmp.com.howtospeak.model;
+
+/**
+ * Created by Dung on 12/24/2017.
+ */
+
+public class Acount {
+    private String name;
+    private String email;
+
+    public Acount(){};
+
+    public String getName(){return name;}
+
+    public void setName(String name){this.name = name;}
+
+    public String getEmail(){return email;}
+
+    public void setEmail(String email){this.email = email;}
+}
